09ffc5592a73407e9e10809aec9bb10b5097926f
KangChayanin <chayanin15632@gmail.com>
2020-10-29 15:57:10 +0700

    29-10-2020

--- Unity/NSC_GAME/Assets/playercon.cs
+++ Unity/NSC_GAME/Assets/playercon.cs
@@ -3,35 +3,75 @@ using System;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class playercon : MonoBehaviour
 {
 
-    public Animator animator;
+    public Animator mAnimator;
     public Camera cam;
-    public NavMeshAgent agent;
-    float InputX;
-    float InputY;
-    float speed;
+    public NavMeshAgent mNavMeshAgent;
     float lastPosition;
-   
     // Update is called once per frame
+
+    public bool mRunning = false;
+    public bool mReverse = false;
+
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+        //mNavMeshAgent.updateRotation = false;
+    }
+
     void Update()
     {
-        if (agent.remainingDistance>0.25f)
-            InputX = -1f;
-        else
-            InputX = 0f;
+        //agent.updateRotation = false;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            mRunning = true;
+        }
+        if(Input.GetKeyUp(KeyCode.Return))
+            mAnimator.speed = 1;
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-           if( Physics.Raycast(ray, out hit))
+           if( Physics.Raycast(ray, out hit, 100))
             {
-                agent.SetDestination(hit.point);
+                mNavMeshAgent.SetDestination(hit.point);
             }
-
         }
-        animator.SetFloat("InputX", InputX);
+
+        //
+        //transform.rotation = Quaternion.LookRotation(mNavMeshAgent.velocity.normalized);
+        //
+
+        if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            mRunning = false;
+        }
+        else
+            mRunning = true;
+
+
+
+        if (mRunning == false)
+            mNavMeshAgent.updatePosition = false;
+        else
+            mNavMeshAgent.updatePosition = true;
+
+
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            mAnimator.SetFloat("Speed", -1f);
+            mAnimator.SetBool("running", true);
+        }
+        else
+        {
+            mAnimator.SetBool("running", mRunning);
+            mAnimator.SetFloat("Speed", 1f);
+        }
     }
 }
