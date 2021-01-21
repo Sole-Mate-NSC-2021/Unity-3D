3cf423f5e9bb2a679697c8aab80cba8803ade5cb
PanaWanit <pnaw.wan@gmail.com>
2021-01-21 12:14:59 +0700

    Among Us?

--- Unity/NewNSC01/Assets/Code/ChangeScene.cs
+++ Unity/NewNSC01/Assets/Code/ChangeScene.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public static bool _is_change=false;
+    public static string scenename;
     public void chage_to_select_menu() {
+        scenename = "Select_Stage";
+        GameObject.Find("RawImage").GetComponent<Animator>().Play("Fade_in");
+        _is_change=true;
+    }
+    public void change_to_setting() {
+        scenename="Setting";
         GameObject.Find("RawImage").GetComponent<Animator>().Play("Fade_in");
         _is_change=true;
     }
--- Unity/NewNSC01/Assets/Code/Fade_change.cs
+++ Unity/NewNSC01/Assets/Code/Fade_change.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class Fade_change : MonoBehaviour
-{
+public class Fade_change : MonoBehaviour {
+
     void Update() {
         if(gameObject.GetComponent<RawImage>().color.a == 1f) {
-            SceneManager.LoadScene("Select_Stage");
+            SceneManager.LoadScene(ChangeScene.scenename);
         }
     }
 }
