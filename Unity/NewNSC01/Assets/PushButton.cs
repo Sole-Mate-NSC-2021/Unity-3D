674108a84e7dfedb805c43f322cce0bf970e2946
Aphrodicez <57552210+Aphrodicez@users.noreply.github.com>
2021-02-06 11:26:59 +0700

    Push

--- Unity/NewNSC01/Assets/PushButton.cs
+++ Unity/NewNSC01/Assets/PushButton.cs
@@ -11,6 +11,12 @@ public class PushButton : MonoBehaviour
 
     PlayerControl playerControl;
 
+    Vector3 lastRot, toRot, currentRot;
+
+    int i, j;
+
+    int ch = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +28,8 @@ public class PushButton : MonoBehaviour
         Door[1, 4] = RedDoor;
         Door[1, 3] = BlueDoor;
 
+        toRot = new Vector3(0, 0, 0);
+
         playerControl = GetComponent<PlayerControl>();
 
     }
@@ -29,6 +37,33 @@ public class PushButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        /*
+        if (ch == 2)
+        {
+            currentRot = Vector3.Lerp(currentRot, toRot, 2 * Time.deltaTime);
+            transform.eulerAngles = currentRot;
+            if (transform.eulerAngles == )
+            {
+                Debug.Log("HASDA");
+                bool isActive = Door[i, j].activeSelf;
+                Door[i, j].SetActive(!isActive);
+                ch = 1;
+                currentRot = transform.eulerAngles;
+            }
+        }
+
+        else if (ch == 1)
+        {
+            Debug.Log("AHA");
+            currentRot = Vector3.Lerp(currentRot, lastRot, 2 * Time.deltaTime);
+            transform.eulerAngles = currentRot;
+            if (transform.eulerAngles == lastRot)
+            {
+                ch = 0;
+            }
+        }
+        */
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray;
@@ -41,12 +76,24 @@ public class PushButton : MonoBehaviour
                 if(hit.collider.tag == "Button")
                 {
                     ButtonPressed = hit.collider.gameObject;
-                    int i = ButtonPressed.name[6] - '0';
-                    int j = ButtonPressed.name[7] - '0';
+                    i = ButtonPressed.name[6] - '0';
+                    j = ButtonPressed.name[7] - '0';
                     if(playerControl.StartI == i && playerControl.StartJ == j)
                     {
-                        bool isActive = Door[i, j].activeSelf;
-                        Door[i, j].SetActive(!isActive);
+                        lastRot = transform.eulerAngles;
+                        /*
+                        ch = 2;
+                        currentRot = lastRot;
+                        if(lastRot.y > 180)
+                        {
+                            toRot = new Vector3(0, 360, 0);
+                        }
+                        else
+                        {
+                            toRot = new Vector3(0, 0, 0);
+                        }
+                        */
+                        //transform.eulerAngles = lastRot;
                     }
                 }
             }
