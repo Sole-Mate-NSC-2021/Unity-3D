491f19e83a1033e679a1a76908d02ee5bb51fec6
Aphrodicez <57552210+Aphrodicez@users.noreply.github.com>
2021-02-24 08:27:29 +0700

    push

---
+++ Unity/NewNSC01/Assets/CamControl2.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl2 : MonoBehaviour
+{
+    public float panSpeed;
+    public float ScreenBoarder = 10f;
+
+    public Vector3 dragOrigin, dragPos, nextPos, realPos, tmpPos;
+
+    public float leftX, rightX, leftY, rightY;
+
+    void Start()
+    {
+        panSpeed = 1.0f;
+        leftX = 31;
+        rightX = -15;
+        leftY = 14;
+        rightY = 23;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1))
+            return;
+
+        tmpPos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        dragPos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+        nextPos = new Vector3(tmpPos.x * panSpeed, dragPos.y * panSpeed, 0);
+        realPos = Camera.main.ViewportToWorldPoint(nextPos);
+
+        //realPos.x = Mathf.Max(realPos.x, leftX);
+        /*
+        realPos.x = Mathf.Min(realPos.x, rightX);
+
+        realPos.y = Mathf.Max(realPos.y, leftY);
+        realPos.y = Mathf.Min(realPos.y, rightY);
+        */
+
+        //realPos.x = Mathf.Min(realPos.x, 20.0f);
+        //realPos.y = Mathf.Min(realPos.y, 13.5f);
+
+        //Debug.Log(realPos.y);
+
+        //Debug.Log("HAHS" + transform.position.y);
+
+        nextPos = Camera.main.WorldToViewportPoint(realPos);
+        nextPos.z = Camera.main.ScreenToViewportPoint(dragOrigin).z;
+        transform.Translate(nextPos, Space.World);
+        transform.position = new Vector3(Mathf.Min(19.0f, transform.position.x), Mathf.Max(19.0f, transform.position.y), transform.position.z);
+        transform.position = new Vector3(Mathf.Max(19.0f, transform.position.x), Mathf.Min(25.5f, transform.position.y), transform.position.z);
+    }
+}
