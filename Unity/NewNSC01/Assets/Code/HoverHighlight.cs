a5525b1f9fc4af3dc6ac1f3770dc3240ba81a9ad
Aphrodicez <57552210+Aphrodicez@users.noreply.github.com>
2021-02-24 19:37:50 +0700

    Finish All

--- Unity/NewNSC01/Assets/Code/HoverHighlight.cs
+++ Unity/NewNSC01/Assets/Code/HoverHighlight.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class HoverHighlight : MonoBehaviour
 {
 
+    public GameObject Border;
+
     //public GameObject Border;
     void Start()
     {
         if(this.tag == "Dot")
             this.GetComponent<BoxCollider>().size = new Vector3(10, 10, 10);
-        //Border = GameObject.Find(this.gameObject.name + "_Border");
+        Border = GameObject.Find(this.gameObject.name + "_Border");
     }
     void OnMouseOver()
     {
-        //Debug.Log("HASD");
+        if(this.tag == "Box")
+        {
+            Border.GetComponent<Renderer>().enabled = true;
+            return;
+        }
+        //Debug.Log(this.name);
         this.GetComponent<Renderer>().enabled = true;
         //Border.GetComponent<Renderer>().enabled = true;
     }
     void OnMouseExit()
     {
+        if (this.tag == "Box")
+        {
+            Border.GetComponent<Renderer>().enabled = false;
+            return;
+        }
         this.GetComponent<Renderer>().enabled = false;
         //Border.GetComponent<Renderer>().enabled = false;
     }
